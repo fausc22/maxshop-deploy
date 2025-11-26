78c73ce48edfeafa8d7ad89ff58d1ef02b671177
fausc22 <faausc@gmail.com>
2025-11-26 09:12:08 -0300

    cors

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(helmet());
 app.use(cors({
-    origin: [process.env.FRONTEND_URL || 'http://localhost:5173', 'http://192.168.0.13:3000'],
+    origin: [process.env.FRONTEND_URL || 'https://maxshop-front.vercel.app', 'http://192.168.0.13:3000'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
